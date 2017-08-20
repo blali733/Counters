c988c6bdcd540cd9b682314ae80169914baacc39
blali733 <blali733@gmail.com>
2017-08-20 20:17:18 +0200

    Rework of CounterElement to fit sharing plan.

--- app/src/main/java/pl/blali733/counters/CounterElement.java
+++ app/src/main/java/pl/blali733/counters/CounterElement.java
@@ -1,7 +1,17 @@
 package pl.blali733.counters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CounterElement {
     String label;
     int v1,v2;
     boolean mixed;
+    List<String> shared;
+    boolean dirty;
+
+    public CounterElement(){
+        shared = new ArrayList<>();
+        dirty = true;
+    }
 }
